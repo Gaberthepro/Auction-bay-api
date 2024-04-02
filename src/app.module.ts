d4aa17e83d497c689b640a65f133241674657036
GaberČuješ <gabercujes@gmail.com>
2024-04-02 09:33:29 +0200

    connection to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    password: 'gaber',
+    username: 'postgres',
+    entities: [],
+    database: 'auctionbay',
+    synchronize: true,
+    logging: true,
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
