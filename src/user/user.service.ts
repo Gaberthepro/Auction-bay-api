8e0f124c626076f950cf76d7a5f24b278b95347a
GaberČuješ <gabercujes@gmail.com>
2024-04-04 13:44:12 +0200

    Bycrypt password hash

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,14 +4,22 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
-  create(createUserDto: CreateUserDto): Promise<User> {
-    return this.userRepository.save(createUserDto);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+    const user: User = new User();
+    user.name = createUserDto.name;
+    user.surname = createUserDto.surname;
+    user.email = createUserDto.email;
+    user.password = await bcrypt.hash(createUserDto.password, saltOrRounds);
+    user.imgURl = '';
+    return this.userRepository.save(user);
   }
 
   findAll(): Promise<User[]> {
@@ -31,10 +39,9 @@ export class UserService {
     user.imgURl = updateUserDto.imgURl;
     user.id = id;
     return this.userRepository.save(user);
-
   }
 
-  remove(id: number):Promise<{ affected?: number }> {
+  remove(id: number): Promise<{ affected?: number }> {
     return this.userRepository.delete(id);
   }
 }
