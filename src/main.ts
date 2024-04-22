4c31be48fb9437cb284039067217dd12d07ca5c1
GaberČuješ <gabercujes@gmail.com>
2024-04-22 13:30:15 +0200

    origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+    ],
   });
   await app.listen(3000);
 }
