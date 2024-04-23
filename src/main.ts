3614e3d76bef28c29ec1db0a304fd0aed137828b
GaberČuješ <gabercujes@gmail.com>
2024-04-23 16:38:47 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
       'http://localhost:5173',
+      'https://auctionbay.vercel.app',
+      'http://localhost:4173',
     ],
   });
   await app.listen(3000);
